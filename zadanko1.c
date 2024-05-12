d80df9107d91b31943d74576925c1a949a941cdc
Super Student Knur Campa <student@knur.club>
2024-05-12 14:18:38 +0000

    dodano opis funkcji weryfikacja

--- zadanko1.c
+++ zadanko1.c
@@ -3,32 +3,26 @@
 #include <stdint.h>
 #include <string.h>
 
-void weryfikacja1(void* ss, int sz)
-{
-    char* st = (char*)ss;
+void weryfikacja1(void *ss, int sz) {
+    char *st = (char *) ss;
     int wiek = *st;
-    if(wiek != 105)
-    {
+    if (wiek != 105) {
         printf("BĹ‚Ä™dne rozwiÄ…zanie\n");
         return;
     }
     st += sz / 3;
-    uint64_t imiePtr = (*(uint64_t*)st);
-    char* imie = (char*)imiePtr;
+    uint64_t imiePtr = (*(uint64_t *) st);
+    char *imie = (char *) imiePtr;
     st += sz / 3;
 
-    if(strcmp(imie, "Jan") != 0)
-    {
+    if (strcmp(imie, "Jan") != 0) {
         printf("BĹ‚Ä™dne rozwiÄ…zanie\n");
         return;
     }
-    float wzrost = *(float*)st;
-    if(wzrost < 2.138 && wzrost > 2.136)
-    {
+    float wzrost = *(float *) st;
+    if (wzrost < 2.138 && wzrost > 2.136) {
         printf("Brawo poprawne rozwiÄ…zanie\n");
-    }
-    else
-    {
+    } else {
         printf("Ups, bĹ‚Ä™dne rozwiÄ…zanie\n");
     }
 }
@@ -52,14 +46,43 @@ void weryfikacja1(void* ss, int sz)
  Opisz co robi po kolei funckja `void weryfikacja1(void* ss, int sz)`
 */
 
-int main(void)
-{
+/*
+ * Opis działania funkcji weryfikacja1:
+ *
+ * Nie wiem czy dobrze, ucze się dopiero. Bardzo prosze o wytłumaczenie nieścisłości jeżeli można.
+ * W celu przeprowadzenia analizy i opisu funkcji pożytkowałem się tym co usłyszałem na wykładzie + dokumentacja + artykuły.
+ *
+ *
+ * Parametry: (void *ss) -> "wskazuje na adres przekazany w argumencie podczas wywołania funkcji."
+ *            (int sz) -> "przyjumuje wartość która zwracana jest przez sizeof(Jan) przekazana w argumencie podczas wywołania funkcji."
+ * Ciało:
+ *            char *st = (char *) ss; -> "Tworzony jest wskaźnik do przekazanej w argumencie zmiennej ss gdzie następuje type cast na typ char
+ *                                       ze względu na to że nie możemy zdereferować typu void."
+ *            int wiek = *st -> "Tworzona jest zmienna wiek która przyjmuje wartość pod adresem na który wskazuje st."
+ *                              (Nie rozumiem dlaczego *st wskazuje akurat na wiek naszego Jana, ale tak to wynika z kodu o ile dobrze rozumuje (pewnie nie XD))
+ *            st += sz / 3 -> "sizeof(Jan) zwraca liczbe 24 co oznacza że st przyjmuje teraz wartość 16."
+ *
+ *            uint64_t imiePtr = (*(uint64_t *) st) -> "Tworzona jest zmienna imiePtr o wielkości 8 bajtów (obstawiam że to z powodu tego że string zajmuje 8 bajtów pamięci), której wartość to dereferencja wskaźnika st,
+ *                                                      który na dodatek jest type castowany na unit64_t (z tego co wynika rozumiem że jeśli zmienna jest np. typu int,
+ *                                                      a nasz wskaźnik np. typu char to żeby przypisać wartość z pod adresu na która wskazuje wskaźnik typu char musimy
+ *                                                      zrobić type cast wskaźnika na int)."
+ *            char *imie = (char *) imiePtr -> "Tworzony jest zmienna wskaźnikowa imie do której przypisana jest zmienna imiePtr z type castem na char."
+ *
+ *            st += sz / 3 -> "st przyjumuje teraz wartość 24 (Obstawiam że to wszystko powiązane jest z dostawaniem się do poszczególnych miejsc w pamięci naszych pól struktury Jana)."
+ *                            (Tylko dlaczego sizeof(Jan) zwraca 24 skoro int i float zajmują po 4 bajty co daje nam 8 ,a char będąć w tym przypadku stringiem zajmuje 8 bajtów.
+ *                            Matematyka podpowiada że sizeof(Jan) powinno zwracać 16, ale myśle że jest coś czego nie rozumiem)
+ *            float wzrost = *(float *) st -> "Tworzona jest zmienna wzrost która przyjmuje wartość z pod adresu na którą wskazuje st, a st jest type castowane na float."
+ *
+ *            Przypuszczam że pola Jana miały być deklarowane w takiej a nie innej kolejności którą zakłada polecenie zadania właśnie dlatego aby działanie st += sz / 3 miało sens,
+ *            ale nie jestem co do tego pewny na 100%.
+ */
+
+int main(void) {
     // Miejce na twĂłj kod
 
-    struct czlowiek
-    {
+    struct czlowiek {
         int wiek;
-        char* imie;
+        char *imie;
         float wzrost;
     };
 
